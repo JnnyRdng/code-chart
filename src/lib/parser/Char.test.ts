f4ecb38ce3f217f76074fd6da8d05015ae3b7072
Jonny Reading <62183163+JnnyRdng@users.noreply.github.com>
2023-09-29 19:01:48 +0100

    temporarily add a failing test to see what happens

--- src/lib/parser/Char.test.ts
+++ src/lib/parser/Char.test.ts
@@ -4,6 +4,10 @@ describe('Char tests', () => {
 
   describe('Constructor', () => {
 
+    it('temporary failing test', () => {
+      expect(1).toStrictEqual(4);
+    });
+
     it('constructs with a single character', () => {
       expect(() => new Char('a')).not.toThrowError();
     });
