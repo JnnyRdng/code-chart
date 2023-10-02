f2d375e961c9b1b73bddc8f40623580b008666d2
Jonny Reading <jonnyreading@gmail.com>
2023-10-02 22:02:10 +0100

    add isUpdating flag to useDebounce

--- src/hooks/useDebounce.test.tsx
+++ src/hooks/useDebounce.test.tsx
@@ -15,33 +15,33 @@ describe('useDebounce tests', () => {
 
   it('returns the initial value', () => {
     const { result } = getHook('initial');
-    const actual = result.current;
+    const [actual] = result.current;
     expect(actual).toStrictEqual('initial');
   });
 
   it('is a generic type, boolean', () => {
     const { result: booleanResult } = getHook(true);
-    expect(typeof booleanResult.current).toStrictEqual('boolean');
+    expect(typeof booleanResult.current[0]).toStrictEqual('boolean');
   });
 
   it('is a generic type, string', () => {
     const { result: stringResult } = getHook('true');
-    expect(typeof stringResult.current).toStrictEqual('string');
+    expect(typeof stringResult.current[0]).toStrictEqual('string');
   });
 
   it('is a generic type, number', () => {
     const { result: numberResult } = getHook(15);
-    expect(typeof numberResult.current).toStrictEqual('number');
+    expect(typeof numberResult.current[0]).toStrictEqual('number');
   });
 
   it('by default it should update after 500 ms', () => {
     const { result, rerender } = getHook('initial');
     rerender({ value: 'newValue', delay: undefined });
-    expect(result.current).toStrictEqual('initial');
+    expect(result.current[0]).toStrictEqual('initial');
     act(() => jest.advanceTimersByTime(499));
-    expect(result.current).toStrictEqual('initial');
+    expect(result.current[0]).toStrictEqual('initial');
     act(() => jest.advanceTimersByTime(1));
-    expect(result.current).toStrictEqual('newValue');
+    expect(result.current[0]).toStrictEqual('newValue');
   });
 
   it('updates immediately if the delay is set to 0', () => {
@@ -49,40 +49,40 @@ describe('useDebounce tests', () => {
     rerender({ value: 'newValue', delay: 0 });
     // progress time by 0 to allow for state update
     act(() => jest.advanceTimersByTime(0));
-    expect(result.current).toStrictEqual('newValue');
+    expect(result.current[0]).toStrictEqual('newValue');
   });
 
   it('if delay is set it takes that long to update', () => {
     const { result, rerender } = getHook('initial', 50);
     rerender({ value: 'newValue', delay: 50 });
-    expect(result.current).toStrictEqual('initial');
+    expect(result.current[0]).toStrictEqual('initial');
     act(() => jest.advanceTimersByTime(49));
-    expect(result.current).toStrictEqual('initial');
+    expect(result.current[0]).toStrictEqual('initial');
     act(() => jest.advanceTimersByTime(1));
-    expect(result.current).toStrictEqual('newValue');
+    expect(result.current[0]).toStrictEqual('newValue');
   });
 
   it('it keeps debouncing the value if the value is repeatedly updated', () => {
     const { result, rerender } = getHook('first', 500);
-    expect(result.current).toStrictEqual('first');
+    expect(result.current[0]).toStrictEqual('first');
     act(() => jest.advanceTimersByTime(400));
-    expect(result.current).toStrictEqual('first');
+    expect(result.current[0]).toStrictEqual('first');
     // update value before delay time is up. The delay will be restarted.
     rerender({ value: 'second', delay: 500 });
-    expect(result.current).toStrictEqual('first');
+    expect(result.current[0]).toStrictEqual('first');
     act(() => jest.advanceTimersByTime(400));
-    expect(result.current).toStrictEqual('first');
+    expect(result.current[0]).toStrictEqual('first');
     // update value again before delay time is up.
     rerender({ value: 'third', delay: 500 });
-    expect(result.current).toStrictEqual('first');
+    expect(result.current[0]).toStrictEqual('first');
     act(() => jest.advanceTimersByTime(400));
-    expect(result.current).toStrictEqual('first');
+    expect(result.current[0]).toStrictEqual('first');
     act(() => jest.advanceTimersByTime(99));
     // With 1ms to go, it's still on the first value
-    expect(result.current).toStrictEqual('first');
+    expect(result.current[0]).toStrictEqual('first');
     act(() => jest.advanceTimersByTime(1));
     // Now on the third value, it never updated to the second.
-    expect(result.current).toStrictEqual('third');
+    expect(result.current[0]).toStrictEqual('third');
   });
 });
 
--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from 'react'
 
-export function useDebounce<T>(value: T, delay?: number): T {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+/**
+ * 
+ * @param value generic. the value to debounce
+ * @param delay optional. the delay after which, with no additional updates, the value should be changed
+ * @returns  tuple containing: [the debounced value, boolean indicating whether an update is pending]
+ */
+export function useDebounce<T>(value: T, delay?: number): [T, boolean] {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+  const [isUpdating, setUpdating] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay ?? 500)
+    setUpdating(true);
+    const timer = setTimeout(() => {
+      setUpdating(false);
+      setDebouncedValue(value);
+    }, delay ?? 500)
 
     return () => {
       clearTimeout(timer)
     }
   }, [value, delay])
 
-  return debouncedValue
+  return [debouncedValue, isUpdating];
 }
